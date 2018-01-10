2aa15c36012b334078b8f62d178b368bd56104b9
Stefan <pralka.gniewu@gmail.com>
2018-01-11 00:34:41 +0100

    Rewrote using vectors

--- main.cpp
+++ main.cpp
@@ -2,11 +2,14 @@
 #include <fstream>
 #include <iomanip>
 #include <cstdlib>
-
+#include <vector>
 
 using namespace std;
 
 // getch for linux 
+// If you're using Windows just uncomment the next line and delete the function;
+
+// #include <conio.h>
 
 #include <unistd.h>
 #include <termios.h>
@@ -53,29 +56,32 @@ int N_value_in()
 	return N;
 }
 
-int N=N_value_in();
+int N = N_value_in();
 
-void N_value_out(int N)
+void N_value_out(int a)
 {
 	ofstream plik;
 	plik.open("n",ios_base::out);
-	plik << N;
+	plik << a;
 	plik.close();
 }
 
-void read_from_file(Data a[])
+vector<Data> read_from_file()
 {
+	vector<Data> a;
 	ifstream plik;
 	plik.open("data.txt",ios_base::in);
 	for(int i=0;i<N;i++)
 	{
+		a.push_back(Data());
 		plik >> a[i].name >> a[i].tally;
 		cout << endl;
 	}
 	plik.close();
+	return a;
 }
 
-void save(Data a[])
+void save(vector<Data> a)
 {
 	ofstream plik;
 	plik.open("data.txt",ios_base::out);
@@ -89,13 +95,13 @@ void save(Data a[])
 Data read()
 {
 	Data a;
-	cout << "Podaj nazwe: ";cin.getline(a.name,32);
+	cout << "Podaj nazwe: "; cin.getline(a.name,32);
 	a.tally=0;
 	return a;
 }
 
 
-void wys(Data a[])
+void wys(vector<Data> a)
 {
 	system("clear");
 	for(int i=0;i<N;i++)
@@ -104,23 +110,27 @@ void wys(Data a[])
 	}
 }
 
-void wipe()
+void wipe(vector<Data> a)
 {
 	ofstream data,n;
 	data.open("data.txt",ios_base::out);
 	data << "";
 	data.close();
+	for(int i=0;i<N;i++)
+	{
+		a.pop_back();
+	}
 	N=0;
 	cout << "Data wipe successful.";
 	getch();
 }
 
-void menu(Data a[])
+void menu(vector<Data> a)
 {
 	char c;
 	do
 	{
-		read_from_file(a);
+		a = read_from_file();
 		system("clear");
 		cout << "       Menu       " << endl << setw(3) << "1. " << "Pokaz." << endl << "2. " << "Dodaj."
 			 << endl << "3. " << "Dodaj do numeru. " << endl << "4. " << "Data wipe. " << endl << "5. " << "Wyjdz." << endl;
@@ -129,25 +139,36 @@ void menu(Data a[])
 		 {
 		 	case '1':
 		 	{
-		 		wys(a);
-		 		getch();
+		 		system("clear");
+		 		if(a.size()==0)
+		 		{
+		 			cout << "There's nothing to display.";
+		 			getch();
+		 		}
+		 		else 
+		 		{
+		 			wys(a);
+		 			getch();
+		 		}
 		 		break;
 		 	}
 		 	case '2':
 		 	{
 		 		system("clear");
+		 		a.push_back(Data());
+		 		int i = a.size()-1;
+		 		a[i]=read();
 		 		N++;
-
-		 		a[N-1]=read();
 		 		save(a);
 		 		break;
 		 	}
 		 	case '3':
 		 	{
-		 		system("clear");
+		 		system("clear");								
 		 		wys(a);
 		 		int i;
 		 		cout << "Gdzie chcesz dodac: "; cin >> i;
+		 		cin.get();
 		 		a[i-1].tally++;
 		 		save(a);
 		 		break;
@@ -155,20 +176,25 @@ void menu(Data a[])
 		 	case '4':
 		 	{
 		 		system("clear");
-		 		wipe();
+		 		wipe(a);
+		 		break;
+		 	}
+		 	case '5':
+		 	{
+		 		N_value_out(N);
+		 		c='q';
 		 		break;
 		 	}
 		 }
-	}while(c!='5');
+	}while(c!='q');
 }
-// This is a comment
+
 
 int main()
 {
-	Data* a = new Data[1024];
+	// Data* a = new Data[1024];
+	vector<Data> a;
 	menu(a);
-	N_value_out(N);
 	system("clear");
-	delete [] a;
 	return 0;
-}// this is another comment
+}
